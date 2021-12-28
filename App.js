e979ad9e611caad0182d036e7082311ef3dddb3d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-12-28 19:32:41 +0100

    Testing out each Touchables

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
 import React, {Component} from 'react';
-import {View} from 'react-native'
-import AddEntry from './components/AddEntry';
+import {View, Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback,} from 'react-native'
+// import AddEntry from './components/AddEntry';
 
 class App extends Component {
+  handlePress =()=> {
+    alert('Hello!');
+  }
   render() {
     return (
-      <View >
-        {/* <Text>Hello World</Text> */}
-        <AddEntry />
+      <View style={styles.container} >
+        {/* <AddEntry /> */}
+        <TouchableOpacity onPress={this.handlePress} underlayColor='#D4271b' style={styles.btn} >
+          <Text style={styles.btnText}>Touchable HIghlight</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#fff',
+  }
+})
+
 
 export default App
 
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import { Text, View } from "react-native";
 import { getMetricMetaInfo } from '../utils/helpers';
+import UdaciSlidder from './UdaciSlidder';
+import UdaciStepper from './UdaciStepper';
+import DateHeader from './DateHeader'
 // import { getMetricMetaInfo } from "../utils/helpers";
 
 class AddEntry extends Component {
@@ -11,7 +14,7 @@ class AddEntry extends Component {
     sleep: 0,
     eat: 0,
   }
-  increment =(metrics)=> {
+  increment =(metric)=> {
     const { max, step } = getMetricMetaInfo(metric);
     this.setState((prevState)=>{
       const count = prevState[metric] + step
@@ -22,7 +25,7 @@ class AddEntry extends Component {
     })
   }
 
-  decrement =(metrics)=> {
+  decrement =(metric)=> {
     const { step } = getMetricMetaInfo(metric);
     this.setState((prevState)=>{
       const count = prevState[metric] - step
@@ -32,20 +35,47 @@ class AddEntry extends Component {
       } 
     })
   }
+
   slide =(metric, value)=> {
-    this.setState((prevState)=>({
+    this.setState(()=>({
       [metric]: value,
     }))
   }
+
   render() {
+    const metaInfo = getMetricMetaInfo();
+    console.log('|||||',metaInfo);
     return (
       <View>
-       {getMetricMetaInfo('bike').getIcon()}
+        <DateHeader date={(new Date()).toLocaleDateString()}/>
+       {Object.keys(metaInfo).map((key)=>{
+        const {getIcon, type, ...rest} = metaInfo[key];
+        const value = this.state[key]; 
+        return (
+          <View key={key}>
+            {getIcon()}
+            {
+              type === 'slider'
+              ?
+              <UdaciSlidder
+                value={value}
+                onChange={(value) => this.slide(key, value)}
+                {...rest}
+            />
+              :<UdaciStepper
+                value={value}
+                onIncrement={() => this.increment(key)}
+                onDecrement={() => this.decrement(key)}
+                {...rest}
+            />
+            }
+          </View>
+        )
+      })}
       </View>
     )
   }
 }
 
+
 export default AddEntry;
-
-
